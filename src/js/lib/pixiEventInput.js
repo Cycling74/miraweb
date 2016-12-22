a57d1290051692baba4d27e8637fd3ef56f242c6
Florian Demmer <florian@cycling74.com>
2016-12-22 12:21:04 +0100

    example adjustments to PixiEventInput in order to showcase Pointer vs. Mouse vs. Touch Input problems when moving to exclusive Pixi PointerEvents
    see #9

--- src/js/lib/pixiEventInput.js
+++ src/js/lib/pixiEventInput.js
@@ -1,17 +1,5 @@
 import Hammer from "hammerjs";
 import findIndex from "lodash/findIndex.js";
-import toArray from "lodash/toArray.js";
-import size from "lodash/size.js";
-
-const PIXI_INPUT_MAP = Object.freeze({
-	touchstart : Hammer.INPUT_START,
-	touchmove : Hammer.INPUT_MOVE,
-	touchend : Hammer.INPUT_END,
-	touchcancel : Hammer.INPUT_CANCEL,
-	mousemove : Hammer.INPUT_MOVE,
-	mouseup : Hammer.INPUT_END,
-	mousedown : Hammer.INPUT_START
-});
 
 const POINTER_INPUT_MAP = Object.freeze({
 	pointerdown : Hammer.INPUT_START,
@@ -35,14 +23,8 @@ const IE10_POINTER_TYPE_ENUM = Object.freeze({
 	4 : INPUT_TYPE.MOUSE
 });
 
-const MOUSE_EVENTS = ["mousemove", "mousedown", "mouseup"];
-const TOUCH_EVENTS = ["touchstart", "touchmove", "touchend", "touchendoutside", "touchcancel"];
 const POINTER_EVENTS = ["pointerdown", "pointermove", "pointerup", "pointerupoutside", "pointercancel"];
-const PIXI_TARGET_EVENTS = [TOUCH_EVENTS.join(" "), MOUSE_EVENTS.join(" ")].join(" ");
-
-function touchSorter(ta, tb) {
-	return ta.identifier - tb.identifier;
-}
+const PIXI_TARGET_EVENTS = POINTER_EVENTS.join(" ");
 
 /**
  * PIXI touch events input
@@ -62,13 +44,7 @@ export default class PixiEventInput extends Hammer.Input {
 	}
 
 	_handler(ev) {
-		if (POINTER_EVENTS.indexOf(ev.data.originalEvent.type) > -1) {
-			this._handlePointerEvent(ev);
-		}
-
-		else {
-			this._handleMouseTouchEvent(ev);
-		}
+		this._handlePointerEvent(ev);
 	}
 
 	_handlePointerEvent(pixiEvent) {
@@ -76,14 +52,32 @@ export default class PixiEventInput extends Hammer.Input {
 		let { store } = this;
 		let removePointer = false;
 
-		let eventTypeNormalized = ev.type.toLowerCase().replace("ms", "");
+		let eventTypeNormalized = pixiEvent.type.toLowerCase();
 		let eventType = POINTER_INPUT_MAP[eventTypeNormalized];
-		let pointerType = IE10_POINTER_TYPE_ENUM[ev.pointerType] || ev.pointerType;
+
+		let pointerType;
+		let pointerId;
+		if (ev.pointerType) {
+			pointerId = ev.pointerId;
+			pointerType = IE10_POINTER_TYPE_ENUM[ev.pointerType] || ev.pointerType;
+		} else if (ev instanceof MouseEvent) {
+			pointerId = pixiEvent.data.identifier;
+			pointerType = INPUT_TYPE.MOUSE;
+
+			ev.pointerId = pointerId;
+			ev.pointerType = pointerType;
+		} else if (ev instanceof TouchEvent) {
+			pointerId = pixiEvent.data.identifier;
+			pointerType = INPUT_TYPE.TOUCH;
+
+			ev.pointerId = pointerId;
+			ev.pointerType = pointerType;
+		}
 
 		let isTouch = (pointerType === INPUT_TYPE.TOUCH);
 
 		// get index of the event in the store
-		let storeIndex = findIndex(store, ["pointerId", ev.pointerId]);
+		let storeIndex = findIndex(store, ["pointerId", pointerId]);
 
 		// start and mouse must be down
 		if (eventType & Hammer.INPUT_START && (ev.button === 0 || isTouch)) {
@@ -114,78 +108,5 @@ export default class PixiEventInput extends Hammer.Input {
 			// remove from the store
 			store.splice(storeIndex, 1);
 		}
-	}
-
-	_handleMouseTouchEvent(ev) {
-		const type = PIXI_INPUT_MAP[ev.type];
-		let pointerType;
-		let pointers;
-		let changedPointers;
-
-		if (MOUSE_EVENTS.indexOf(ev.type) > -1) {
-			pointerType = "mouse";
-			pointers = [ev.data.originalEvent];
-			changedPointers = [ev.data.originalEvent];
-		} else {
-			pointerType = "touch";
-			const touches = this.getTouches(ev, type);
-			if (!touches) {
-				return;
-			}
-			pointers = touches[0];
-			changedPointers = touches[1];
-		}
-
-		this.callback(this.manager, type, {
-			pointers : pointers,
-			changedPointers : changedPointers,
-			pointerType : pointerType,
-			srcEvent : ev.data.originalEvent
-		});
-	}
-
-	/**
-	 * @this {PixiEventInput}
-	 * @param {Object} ev
-	 * @param {Number} type flag
-	 * @returns {undefined|Array} [all, changed]
-	 */
-	getTouches(ev, type) {
-		const allTouches = ev.data.originalEvent.touches;
-
-		// when there is only one touch, the process can be simplified
-		if (type & (Hammer.INPUT_START | Hammer.INPUT_MOVE) && size(allTouches) === 1) {
-			const allTouchesArray = toArray(allTouches);
-			return [allTouchesArray, allTouchesArray];
-		}
-
-		const changedTouches = ev.data.originalEvent.changedTouches;
-		const targetTouches = [];
-		const changedTargetTouches = [];
-
-		for (let i = 0; i < changedTouches.length; i++) {
-			const touch = changedTouches[i];
-			if (this.manager.isTouchLocked(touch)) {
-				changedTargetTouches.push(touch);
-				targetTouches.push(touch);
-			}
-		}
-
-		if (!changedTargetTouches.length) return undefined;
-
-		// merge targetTouches with changedTargetTouches so it contains ALL touches, including "end" and "cancel"
-		for (let i = 0; i < allTouches.length; i++) {
-			const touch = allTouches[i];
-			if (this.manager.isTouchLocked(touch)) {
-				if (findIndex(changedTargetTouches, ["identifier", touch.identifier]) === -1)
-					{targetTouches.push(touch);}
-			}
-		}
-
-		targetTouches.sort(touchSorter);
-		return [
-			targetTouches,
-			changedTargetTouches
-		];
 	}
 }
