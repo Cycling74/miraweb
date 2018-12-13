01c0f69d162dea02bbf908dda9e007c545669fd5
Florian Demmer <florian@cycling74.com>
2018-12-13 08:33:02 +0000

    updated eslint and make use of eslint-config-c74

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+const OFF = 0;
+const ERROR = 2;
+
+module.exports = {
+  "extends": "c74",
+  "rules" : {
+  },
+  "env" : {
+    "es6": true,
+    "node": true,
+    "browser": true
+	},
+	parser: "babel-eslint",
+	"parserOptions": {
+		"sourceType": "module"
+	}
+}
