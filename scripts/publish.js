878e7f3a4e7884fd5c3f3a761eef753236640cc7
Florian Demmer <florian@cycling74.com>
2018-12-13 13:21:58 +0000

    adjusted publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-
-const { readFile } = require("fs").promises;
 const { join } = require("path");
 const { execSync } = require("child_process");
-
 const { executeMain, REPO_DIR, runAsync } = require("./helpers");
 
 executeMain(async () => {
@@ -17,10 +14,26 @@ executeMain(async () => {
 	}
 
 	await runAsync("yarn", ["run", "test"]);
-	// await runAsync("yarn", ["run", "setversion"]);
+	await runAsync("yarn", ["run", "setversion"]);
 
-	// const newVersion = JSON.parse(await readFile(join(REPO_DIR, "package.json"))).version;
-	// await runAsync("yarn", ["publish", join(REPO_DIR, "packages", "xebra-communicator"), "--new-version", newVersion]);
-	// await runAsync("yarn", ["publish", join(REPO_DIR, "packages", "xebra.js"), "--new-version", newVersion]);
+	const packageInfo = require("../package.json");
+
+	await runAsync("git", [
+		"add",
+		"./package.json",
+		"./packages/xebra-communicator/package.json",
+		"./packages/xebra.js/package.json",
+		"./src/package.json"
+	]);
+
+	await runAsync("git", [
+		"commit",
+		"-m",
+		`"Set Version v${packageInfo.version}"`
+	]);
+
+	await runAsync("git", ["tag", `v${packageInfo.version}`]);
+	await runAsync("yarn", ["publish", join(REPO_DIR, "packages", "xebra-communicator"), "--new-version", packageInfo.version]);
+	await runAsync("yarn", ["publish", join(REPO_DIR, "packages", "xebra.js"), "--new-version", packageInfo.version]);
 
 });
